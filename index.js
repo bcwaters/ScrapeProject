75db8658c9e3a987814426451206f808ce2b668f
Brent Waters <bcwaters@asu.edu>
2019-01-29 15:16:52 -0700

    set up a server to recieve a post request and reply with an image containing scraped result

--- index.js
+++ index.js
@@ -2,17 +2,33 @@
 
 const puppeteer = require('puppeteer');
 
-
-async function scrapeData() {
+async function scrapeData(_businessName, res) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	var businessName = "Banner Health System"
+	var businessName = _businessName
 	
 	await page.goto('https://recorder.maricopa.gov/recdocdata/');
-
+	
+	//select from dropdown
+	await page.select('#ctl00_ContentPlaceHolder1_ddlDocCodes', 'HL')
+	await page.waitForNavigation();
 	//fill out form
+	
+	/*
+	//last name ctl00_ContentPlaceHolder1_txtLname2
+	await page.focus('#ctl00_ContentPlaceHolder1_txtLname2');
+	await page.type('#ctl00_ContentPlaceHolder1_txtLname2', 'smith', { delay: 150 });
+	
+	//first name  ctl00_ContentPlaceHolder1_txtFname2
+	await page.focus('#ctl00_ContentPlaceHolder1_txtFname2');
+	await page.type('#ctl00_ContentPlaceHolder1_txtFname2', 'smith', { delay: 150 });
+	*/
+	//business name
 	await page.focus('#ctl00_ContentPlaceHolder1_txtName2');
-	await page.type('#ctl00_ContentPlaceHolder1_txtName2', 'Fancy', { delay: 150 });
+	await page.type('#ctl00_ContentPlaceHolder1_txtName2', businessName, { delay: 150 });
+	
+
+	await page.screenshot({ path: 'beforeseraching.png' });
   
 	//send request
 	//await page.focus('#ctl00_ContentPlaceHolder1_btnSearchPanel1');
@@ -22,11 +38,14 @@ async function scrapeData() {
 	
 	
 	
+	
 	//await page.$eval('#ctl00_ContentPlaceHolder1_btnSearchPanel1', form => form.submit());
 	//await page.waitFor(2000);
 
-	
-	await page.screenshot({ path: 'CurrentWebPage.png' });
+	var imageString = './images/CurrentWebPageSearch_'
+	imageString += businessName + '.png'
+	await page.screenshot({ path: imageString });
+	res.redirect(imageString)
 
 //business name in form is ctl00_ContentPlaceHolder1_txtName2
 //Document code in form is ctl00_ContentPlaceHolder1_ddlDocCodes default id is HL
@@ -38,4 +57,21 @@ async function scrapeData() {
   await browser.close();
 }
 
-scrapeData();
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+const port = 80
+app.use(express.static('./'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/clientPage.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post('/search', (req, res) => {
+	console.log("post received");
+	console.log(req.body)
+	scrapeData(req.body.business, res)
+});
+
+
+//scrapeData();
