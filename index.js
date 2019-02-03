f91fbc44fae0ff550e7482eac95a162dfe46f9a5
Brent Waters <bcwaters@asu.edu>
2019-02-03 13:15:45 -0700

    trying to get awsState to run on server

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
-const port = 80
+const port = 3000
 app.use(express.static('./'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/clientPage.html'))
 
