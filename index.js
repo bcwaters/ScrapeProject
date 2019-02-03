a7020fec8e9e6c68b77ee8cb1a3bbd6835368dc0
Brent Waters <bcwaters@asu.edu>
2019-02-03 13:19:28 -0700

    trying to get awsState to run on server

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.listen(port, () => {
 	pdfImage.convertPage(0).then(function (imagePath) {
 		console.log("called pdf converteer")
 		fs.existsSync("./convertedPDF.png") // => true
-	}).catch();
+	}).catch(console.log("convert Failed"));
 	console.log('Example app listening on porasdft ${port}!')
 	}
 )
